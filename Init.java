c25aefdc7554ec8b162a0b7d449e3727dd9f3944
whmarti <whmarti@hotmail.com>
2021-02-12 18:59:54 +1300

    Actualizacion Projecto corriendo

--- Init.java
+++
@@ -1,7 +0,0 @@
-public class TestingJenkins{
-    public static void main(string  args[]){
-        system.output.write("Hello");
-         system.output.write("He1");
-    }
-
-}
---
+++ TestingJenkins.java
@@ -0,0 +1,7 @@
+public class TestingJenkins{
+    public static void main(String [] args){
+        System.out.println("Hello");
+    }
+
+}
+    
